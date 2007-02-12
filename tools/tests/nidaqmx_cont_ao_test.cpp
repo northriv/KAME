e64508e87afe0953ca56bb1e9bdf2483bdd7797b
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2007-02-13 01:22:46 +0900

    *** empty log message ***

--- tools/tests/nidaqmx_cont_ao_test.cpp
+++ tools/tests/nidaqmx_cont_ao_test.cpp
@@ -1,3 +1,11 @@
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 4
+#define SIZEOF_VOID_P 4
+#define SIZEOF_SHORT 2
+#define SIZEOF_FLOAT 4
+#define SIZEOF_DOUBLE 8
+#define msecsleep(x) (x)
+
 #include "support.h"
 
 #include <stdint.h>
@@ -13,15 +21,16 @@ void my_assert(char const*s, int d) {
 using std::max;
 using std::min;
 
-static const unsigned int BUF_SIZE_HINT = 8192;
+static const unsigned int BUF_SIZE_HINT =65536*16;
 static const unsigned int CB_TRANSFER_SIZE = (BUF_SIZE_HINT/2);
 #define NUM_AO_CH 2
+#define OVERSAMP_AO 1
 
 int16 m_genBufAO[CB_TRANSFER_SIZE * NUM_AO_CH];
 
 TaskHandle m_taskAO;
 void _CHECK_DAQMX_RET(int ret, const char *file, int line) {
-	if(ret ! = 0) {
+	if(ret != 0) {
 	char str[2048];
 		DAQmxGetExtendedErrorInfo(str, sizeof(str));
 		fprintf(stderr, "%s\n@ %s: %d\n", str, file, line);
@@ -32,23 +41,25 @@ void _CHECK_DAQMX_RET(int ret, const char *file, int line) {
 #define CHECK_DAQMX_RET(ret) _CHECK_DAQMX_RET(ret, __FILE__, __LINE__)
 
 int32
-_genCallBackAO(TaskHandle task, int32 /*type*/, uInt32 num_samps, void *data)
+_genCallBackAO(TaskHandle task, int32 /*type*/, uInt32 transfer_size, void *data)
 {
 	try {
-	 	#define NUM_CB_DIV 2
+	 	#define NUM_CB_DIV (transfer_size/128)
 		for(int cnt = 0; cnt < NUM_CB_DIV; cnt++) {
 			uInt32 num_samps = transfer_size / NUM_CB_DIV;
 				
 			int32 samps;
 
 
-/*				CHECK_DAQMX_RET(DAQmxWriteBinaryI16(m_taskAO, num_samps, false, 0.3, 
-					DAQmx_Val_GroupByScanNumber, &m_genBufAO[m_genBankAO][cnt * num_samps * SAMPS_AO_PER_DO * NUM_AO_CH],
-					 &samps, NULL));
-*/				CHECK_DAQMX_RET(DAQmxWriteRaw(m_taskAO, num_samps, false, 0.3, 
-					&m_genBufAO[cnt * num_samps * NUM_AO_CH],
+				CHECK_DAQMX_RET(DAQmxWriteBinaryI16(m_taskAO, num_samps, false, 0.3, 
+					DAQmx_Val_GroupByScanNumber, &m_genBufAO[cnt * num_samps * NUM_AO_CH],
 					 &samps, NULL));
 
+/*				CHECK_DAQMX_RET(DAQmxWriteRaw(m_taskAO, num_samps, false, -1, 
+					&m_genBufAO[cnt * num_samps * NUM_AO_CH],
+					 &samps, NULL));
+*/				if(samps != num_samps)
+					fprintf(stderr, "!%d != %d\n", samps, num_samps);
 		}
 	}
 	catch (...) {
@@ -59,8 +70,8 @@ int
 main(int argc, char **argv)
 {
 		for(int i = 0; i < CB_TRANSFER_SIZE; i++) {
-			m_genBufAO[2 * i] = lrint(cos(i * 2 * PI / 10) * 5000u);
-			m_genBufAO[2 * i + 1] = lrint(sin(i * 2 * PI / 10) * 20000u);
+			m_genBufAO[2 * i] = lrint(cos(i * 2 * PI / 20) * 5000u);
+			m_genBufAO[2 * i + 1] = lrint(sin(i * 2 * PI / 20) * 20000u);
 		}
 	try {
 	    CHECK_DAQMX_RET(DAQmxCreateTask("", &m_taskAO));
@@ -69,31 +80,42 @@ main(int argc, char **argv)
 	    	-1.0, 1.0, DAQmx_Val_Volts, NULL));
 			
 		//DMA is slower than interrupts!
-//		CHECK_DAQMX_RET(DAQmxSetAODataXferMech(m_taskAO, 
-//	    	formatString("%s/ao0:1", intfAO()->devName()).c_str(),
-//			DAQmx_Val_Interrupts));
+		CHECK_DAQMX_RET(DAQmxSetAODataXferMech(m_taskAO, 
+	    	"Dev1/ao0:1",
+			DAQmx_Val_Interrupts));
 
 		CHECK_DAQMX_RET(DAQmxCfgSampClkTiming(m_taskAO, "",
-			1e6, DAQmx_Val_Rising, DAQmx_Val_ContSamps,
+			15e5, DAQmx_Val_Rising, DAQmx_Val_ContSamps,
 			BUF_SIZE_HINT * OVERSAMP_AO));
 
 		//Buffer setup.
-		CHECK_DAQMX_RET(DAQmxSetAODataXferReqCond(m_taskAO, 
-	    	"Dev1/ao0:1",
-//			DAQmx_Val_OnBrdMemNotFull));
-			DAQmx_Val_OnBrdMemHalfFullOrLess));
+//		CHECK_DAQMX_RET(DAQmxSetAODataXferReqCond(m_taskAO, 
+//	    	"Dev1/ao0:1",
+////			DAQmx_Val_OnBrdMemNotFull));
+//			DAQmx_Val_OnBrdMemHalfFullOrLess));
+//		CHECK_DAQMX_RET(DAQmxSetWriteWaitMode(m_taskAO, DAQmx_Val_Poll));
+
 		CHECK_DAQMX_RET(DAQmxCfgOutputBuffer(m_taskAO, BUF_SIZE_HINT ));
 		uInt32 bufsize;
 		CHECK_DAQMX_RET(DAQmxGetBufOutputBufSize(m_taskAO, &bufsize));
 		printf("Using bufsize = %d\n", (int)bufsize);
 		CHECK_DAQMX_RET(DAQmxGetBufOutputOnbrdBufSize(m_taskAO, &bufsize));
 		printf("On-board bufsize = %d\n", bufsize);
+		
 		CHECK_DAQMX_RET(DAQmxSetWriteRegenMode(m_taskAO, DAQmx_Val_DoNotAllowRegen));
 
-		CHECK_DAQMX_RET(DAQmxRegisterEveryNSamplesEvent(m_taskAO,
+/*		CHECK_DAQMX_RET(DAQmxRegisterEveryNSamplesEvent(m_taskAO,
 			DAQmx_Val_Transferred_From_Buffer, CB_TRANSFER_SIZE, 0,
-			_genCallBackAO, this));
+			_genCallBackAO, NULL));
+*/			
+		_genCallBackAO(m_taskAO,DAQmx_Val_Transferred_From_Buffer, CB_TRANSFER_SIZE,
+			NULL);
+		_genCallBackAO(m_taskAO,DAQmx_Val_Transferred_From_Buffer, CB_TRANSFER_SIZE,
+			NULL);
 	    CHECK_DAQMX_RET(DAQmxStartTask(m_taskAO));
+	    for(unsigned int i = 0; i < 16; i++)
+		_genCallBackAO(m_taskAO,DAQmx_Val_Transferred_From_Buffer, CB_TRANSFER_SIZE,
+			NULL);
 	
 		getchar();
 	}
--- tools/tests/nidaqmx_cont_co_test.cpp
+++ tools/tests/nidaqmx_cont_co_test.cpp
@@ -1,3 +1,11 @@
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 4
+#define SIZEOF_VOID_P 4
+#define SIZEOF_SHORT 2
+#define SIZEOF_FLOAT 4
+#define SIZEOF_DOUBLE 8
+#define msecsleep(x) (x)
+
 #include "support.h"
 
 #include <stdint.h>
@@ -13,8 +21,7 @@ void my_assert(char const*s, int d) {
 using std::max;
 using std::min;
 
-static const unsigned int BUF_SIZE_HINT = 8192;
-static const unsigned int CB_TRANSFER_SIZE = (BUF_SIZE_HINT/2);
+static const unsigned int CB_TRANSFER_SIZE = 16;
 #define NUM_CO_CH 1
 
 uInt32 m_genBufCOLowTicks[CB_TRANSFER_SIZE * NUM_CO_CH];
@@ -22,7 +29,7 @@ uInt32 m_genBufCOHighTicks[CB_TRANSFER_SIZE * NUM_CO_CH];
 
 TaskHandle m_taskCO;
 void _CHECK_DAQMX_RET(int ret, const char *file, int line) {
-	if(ret ! = 0) {
+	if(ret != 0) {
 	char str[2048];
 		DAQmxGetExtendedErrorInfo(str, sizeof(str));
 		fprintf(stderr, "%s\n@ %s: %d\n", str, file, line);
@@ -32,64 +39,61 @@ void _CHECK_DAQMX_RET(int ret, const char *file, int line) {
 }
 #define CHECK_DAQMX_RET(ret) _CHECK_DAQMX_RET(ret, __FILE__, __LINE__)
 
+unsigned int cnt = 0;
+
 int32
-_genCallBackCO(TaskHandle task, int32 /*type*/, uInt32 num_samps, void *data)
+_genCallBackCO(TaskHandle task, int32 signalID, void *data)
 {
-	try {
-	 	#define NUM_CB_DIV 1
-		for(int cnt = 0; cnt < NUM_CB_DIV; cnt++) {
-			uInt32 num_samps = transfer_size / NUM_CB_DIV;
-				
-			int32 samps;
-
-
-				CHECK_DAQMX_RET(DAQmxWriteCtrTicks(m_taskCO, num_samps, false, 0.3, 
-					DAQmx_Val_GroupByScanNumber, 
-					&m_genBufCOHighTicks[cnt * num_samps * NUM_CO_CH],
-					&m_genBufCOLowTicks[cnt * num_samps * NUM_CO_CH],
-					&samps, NULL));
-		}
-	}
-	catch (...) {
-	}
+//	CHECK_DAQMX_RET(DAQmxWriteCtrTicksScalar(m_taskCO, false, 0, 
+//	m_genBufCOHighTicks[cnt],
+//	m_genBufCOLowTicks[cnt],
+//	NULL));
+	CHECK_DAQMX_RET(DAQmxSetCOPulseLowTicks(m_taskCO, "Dev1/ctr0", 
+	m_genBufCOLowTicks[cnt]
+	));
+	cnt++;
+	cnt = cnt % CB_TRANSFER_SIZE;
+	return 0;
 }
-
 int
 main(int argc, char **argv)
 {
 		for(int i = 0; i < CB_TRANSFER_SIZE; i++) {
-			m_genBufCOLowTicks[i] = 1000;
-			m_genBufCOHighTicks[i] = i * 10;
+			m_genBufCOLowTicks[i] = i * 10000 + 200;
+			m_genBufCOHighTicks[i] = 10000;
 		}
 	try {
 		
 	    CHECK_DAQMX_RET(DAQmxCreateTask("", &m_taskCO));
 	
 		CHECK_DAQMX_RET(DAQmxCreateCOPulseChanTicks(m_taskCO, 
-	    	"Dev1/ctr0", "", DAQmx_Val_Hz, DAQmx_Val_Low, 100,
-	    	0, 10));
+	    	"Dev1/ctr0", "", "20MHzTimebase", DAQmx_Val_Low, 100,
+	    	5000, 1000));
 			
-		CHECK_DAQMX_RET(DAQmxCfgSampClkTiming(m_taskCO, "",
-			1e6, DAQmx_Val_Rising, DAQmx_Val_ContSamps,
+		CHECK_DAQMX_RET(DAQmxCfgImplicitTiming(m_taskCO,
+			DAQmx_Val_ContSamps,
+			100));
+/*		CHECK_DAQMX_RET(DAQmxCfgSampClkTiming(m_taskCO,
+			"/Dev2/20MHzTimebase", 20e6, DAQmx_Val_Rising,
+			DAQmx_Val_HWTimedSinglePoint,
 			BUF_SIZE_HINT));
+*/
+		CHECK_DAQMX_RET(DAQmxRegisterSignalEvent(m_taskCO,
+			DAQmx_Val_CounterOutputEvent, 0,
+			_genCallBackCO, NULL));
 
-		CHECK_DAQMX_RET(DAQmxCfgOutputBuffer(m_taskCO, BUF_SIZE_HINT ));
-		uInt32 bufsize;
-		CHECK_DAQMX_RET(DAQmxGetBufOutputBufSize(m_taskCO, &bufsize));
-		printf("Using bufsize = %d\n", (int)bufsize);
-		CHECK_DAQMX_RET(DAQmxGetBufOutputOnbrdBufSize(m_taskCO, &bufsize));
-		printf("On-board bufsize = %d\n", bufsize);
-		CHECK_DAQMX_RET(DAQmxSetWriteRegenMode(m_taskCO, DAQmx_Val_DoNotAllowRegen));
-
-		CHECK_DAQMX_RET(DAQmxRegisterEveryNSamplesEvent(m_taskCO,
-			DAQmx_Val_Transferred_From_Buffer, CB_TRANSFER_SIZE, 0,
-			_genCallBackCO, this));
 	    CHECK_DAQMX_RET(DAQmxStartTask(m_taskCO));
-
-		getchar();
+			
+		for(int cnt = 0; cnt < CB_TRANSFER_SIZE; cnt++) {
+/*			int32 state;
+			do {
+				CHECK_DAQMX_RET(DAQmxGetCOOutputState(m_taskCO, "Dev1/ctr0",  &state));
+			} while (state == DAQmx_Val_High);
+*/		}
 	}
 	catch (...) {
 	}
+    getchar();
     DAQmxStopTask(m_taskCO);
 	DAQmxClearTask(m_taskCO);
 }
