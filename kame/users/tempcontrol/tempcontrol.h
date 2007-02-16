e6edddab99a41e8fc125d244e3db2a47f5698299
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2007-02-17 04:41:54 +0900

    Add copyright message.

--- kame/users/tempcontrol/tempcontrol.h
+++ kame/users/tempcontrol/tempcontrol.h
@@ -1,3 +1,16 @@
+/***************************************************************************
+		Copyright (C) 2002-2007 Kentaro Kitagawa
+		                   kitagawa@scphys.kyoto-u.ac.jp
+		
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU Library General Public
+		License as published by the Free Software Foundation; either
+		version 2 of the License, or (at your option) any later version.
+		
+		You should have received a copy of the GNU Library General 
+		Public License and a list of authors along with this program; 
+		see the files COPYING and AUTHORS.
+ ***************************************************************************/
 #ifndef tempcontrolH
 #define tempcontrolH
 //---------------------------------------------------------------------------
