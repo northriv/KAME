e8fbb7a1600715f1d576fb2eaf6526eb1a87dba3
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2007-03-13 04:35:06 +0900

    *** empty log message ***

---
+++ kame/atomic_prv_ppc.h
@@ -0,0 +1,117 @@
+/***************************************************************************
+		Copyright (C) 2002-2007 Kentaro Kitagawa
+		                   kitagawa@scphys.kyoto-u.ac.jp
+		
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU Library General Public
+		License as published by the Free Software Foundation; either
+		version 2 of the License, or (at your option) any later version.
+		
+		You should have received a copy of the GNU Library General 
+		Public License and a list of authors along with this program; 
+		see the files COPYING and AUTHORS.
+***************************************************************************/
+#ifndef ATOMIC_PRV_PPC_H_
+#define ATOMIC_PRV_PPC_H_
+
+//! memory barriers. 
+inline void readBarrier() {
+	asm volatile( "isync" ::: "memory" );
+}
+inline void writeBarrier() {
+	asm volatile( "osync" ::: "memory" );
+}
+inline void memoryBarrier() {
+	asm volatile( "sync" ::: "memory" );
+}
+//! For spinning.
+inline void pauseN(unsigned int cnt) {
+	for(unsigned int i = cnt; i != 0; --i)
+		asm volatile( "nop" ::: "memory" );
+}
+template <typename T>
+//! \return true if old == *target and new value is assigned
+inline bool atomicCompareAndSet(T oldv, T newv, T *target ) {
+	T ret;
+	asm volatile ( "1: \n"
+				"lwarx %[ret], 0, %[target] \n"
+				"cmpw %[ret], %[oldv] \n"
+				"bne- 2f \n"
+				"stwcx. %[newv], 0, %[target] \n"
+				"bne- 1b \n"
+				"2: "
+				: [ret] "=&r" (ret)
+				: [oldv] "r" (oldv), [newv] "r" (newv), [target] "r" (target)
+				: "cc", "memory");
+	return (ret == oldv);
+}
+//! \return target's old value.
+template <typename T>
+inline T atomicSwap(T newv, T *target ) {
+	T ret;
+	asm volatile ( "1: \n"
+				"lwarx %[ret], 0, %[target] \n"
+				"stwcx. %[newv], 0, %[target] \n"
+				"bne- 1b"
+				: [ret] "=&r" (ret)
+				: [newv] "r" (newv), [target] "r" (target)
+				: "cc", "memory");
+	return ret;
+}
+template <typename T>
+inline void atomicInc(T *target ) {
+	T ret;
+	asm volatile ( "1: \n"
+				"lwarx %[ret], 0, %[target] \n"
+				"addi %[ret], %[ret], 1 \n"
+				"stwcx. %[ret], 0, %[target] \n"
+				"bne- 1b"
+				: [ret] "=&b" (ret)
+				: [target] "r" (target)
+				: "cc", "memory");
+}
+template <typename T>
+inline void atomicDec(T *target ) {
+	T ret;
+	asm volatile ( "1: \n"
+				"lwarx %[ret], 0, %[target] \n"
+				"addi %[ret], %[ret], -1 \n"
+				"stwcx. %[ret], 0, %[target] \n"
+				"bne- 1b"
+				: [ret] "=&b" (ret)
+				: [target] "r" (target)
+				: "cc", "memory");
+}
+template <typename T>
+inline void atomicAdd(T *target, T x ) {
+	T ret;
+	asm volatile ( "1: \n"
+				" lwarx %[ret], 0, %[target] \n"
+				"add %[ret], %[ret], %[x] \n"
+				"stwcx. %[ret], 0, %[target] \n"
+				"bne- 1b"
+				: [ret] "=&r" (ret)
+				: [target] "r" (target), [x] "r" (x)
+				: "cc", "memory");
+}
+//! \return true if new value is zero.
+template <typename T>
+inline bool atomicAddAndTest(T *target, T x ) {
+	T ret;
+	asm volatile ( "1: \n"
+				"lwarx %[ret], 0, %[target] \n"
+				"add %[ret], %[ret], %[x] \n"
+				"stwcx. %[ret], 0, %[target] \n"
+				"bne- 1b"
+				: [ret] "=&r" (ret)
+				: [target] "r" (target), [x] "r" (x)
+				: "cc", "memory");
+	return (ret == 0);
+}
+//! \return zero flag.
+template <typename T>
+inline bool atomicDecAndTest(T *target ) {
+	return atomicAddAndTest(target, (T)-1);
+}
+
+#endif /*ATOMIC_PRV_PPC_H_*/
---
+++ kame/atomic_prv_x86.h
@@ -0,0 +1,143 @@
+/***************************************************************************
+		Copyright (C) 2002-2007 Kentaro Kitagawa
+		                   kitagawa@scphys.kyoto-u.ac.jp
+		
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU Library General Public
+		License as published by the Free Software Foundation; either
+		version 2 of the License, or (at your option) any later version.
+		
+		You should have received a copy of the GNU Library General 
+		Public License and a list of authors along with this program; 
+		see the files COPYING and AUTHORS.
+***************************************************************************/
+#ifndef ATOMIC_PRV_X86_H_
+#define ATOMIC_PRV_X86_H_
+
+//! memory barriers. 
+inline void readBarrier() {
+	asm volatile( "lfence" ::: "memory" );
+}
+inline void writeBarrier() {
+	asm volatile( "sfence" ::: "memory" );
+}
+inline void memoryBarrier() {
+	asm volatile( "mfence" ::: "memory" );
+}
+//! For spinning.
+inline void pauseN(unsigned int cnt) {
+	for(unsigned int i = cnt; i != 0; --i) {
+		asm volatile( "pause" ::: "memory" );
+	}
+}
+//! \return true if old == *target and new value is assigned
+template <typename T>
+inline bool atomicCompareAndSet(T oldv, T newv, T *target ) {
+	register unsigned char ret;
+	asm volatile (
+		"  lock; cmpxchg%z2 %2,%3;"
+		" sete %0" // ret = zflag ? 1 : 0
+		: "=q" (ret), "=a" (oldv)
+		: "r" (newv), "m" (*target), "1" (oldv)
+		: "memory");
+	return ret;
+}
+#if SIZEOF_VOID_P == 4
+#define HAVE_CAS_2
+//! Compare-And-Swap 2 long words.
+//! \param oldv0 compared with \p target[0].
+//! \param oldv1 compared with \p target[1].
+//! \param newv0 new value to \p target[0].
+//! \param newv1 new value to \p target[1].
+inline bool atomicCompareAndSet2(
+	uint32_t oldv0, uint32_t oldv1,
+	uint32_t newv0, uint32_t newv1, uint32_t *target ) {
+	unsigned char ret;
+	asm volatile (
+#ifdef MACOSX
+		//gcc in XCode cannot handle EBX correctly.
+		" push %%ebx;"
+#endif
+		" mov %6, %%ebx;"
+		" lock; cmpxchg8b %7;"
+#ifdef MACOSX
+		" pop %%ebx;"
+#endif
+		" sete %0;" // ret = zflag ? 1 : 0
+		: "=r" (ret), "=d" (oldv1), "=a" (oldv0)
+		: "1" (oldv1), "2" (oldv0),
+		"c" (newv1), "g" (newv0),
+		"m" (*target)
+#ifdef MACOSX
+		: "memory");
+#else
+		: "memory", "%ebx");
+#endif
+	return ret;
+}
+inline bool atomicCompareAndSet2(
+								int32_t oldv0, int32_t oldv1,
+								int32_t newv0, int32_t newv1, int32_t *target ) {
+	return atomicCompareAndSet2((uint32_t) oldv0, (uint32_t) oldv1,
+								(uint32_t) newv0, (uint32_t) newv1, (uint32_t*) target);
+}
+#endif
+template <typename T>
+inline T atomicSwap(T v, T *target ) {
+	asm volatile (
+				"xchg%z0 %0,%1" //lock prefix is not needed.
+				: "=r" (v)
+				: "m" (*target), "0" (v)
+				: "memory" );
+	return v;
+}
+template <typename T>
+inline void atomicAdd(T *target, T x ) {
+	asm volatile (
+				"lock; add%z0 %1,%0"
+				:
+				: "m" (*target), "ir" (x)
+				: "memory" );
+}
+//! \return true if new value is zero.
+template <typename T>
+inline bool atomicAddAndTest(T *target, T x ) {
+	register unsigned char ret;
+	asm volatile (
+				"lock; add%z1 %2,%1;"
+				" sete %0" // ret = zflag ? 1 : 0
+				: "=q" (ret)
+				: "m" (*target), "ir" (x)
+				: "memory" );
+	return ret;
+}
+template <typename T>
+inline void atomicInc(T *target ) {
+	asm volatile (
+				"lock; inc%z0 %0"
+				:
+				: "m" (*target)
+				: "memory" );
+}
+template <typename T>
+inline void atomicDec(T *target ) {
+	asm volatile (
+				"lock; dec%z0 %0"
+				:
+				: "m" (*target)
+				: "memory" );
+}
+//! \return zero flag.
+template <typename T>
+inline bool atomicDecAndTest(T *target ) {
+	register unsigned char ret;
+	asm volatile (
+				"lock; dec%z1 %1;"
+				" sete %0" // ret = zflag ? 1 : 0
+				: "=q" (ret)
+				: "m" (*target)
+				: "memory" );
+	return ret;
+}
+
+#endif /*ATOMIC_PRV_X86_H_*/
