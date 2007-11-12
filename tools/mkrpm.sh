c68eee63eb22ea0387240f54b0822cfe9c82687a
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2007-11-12 15:30:28 +0900

    *** empty log message ***

--- tools/mkrpm.sh
+++ tools/mkrpm.sh
@@ -4,8 +4,6 @@ version=`grep Version ./kame.spec | sed -e 's/Version: //'`
 file=kame-$version
 echo $file
 ./tools/maketarball.sh $file
-target=${2:-i686}
-echo TARGET=$target
 export PATH=/usr/libexec/ccache:/usr/lib/ccache:$PATH 
-rpmbuild --target $target -ta ../2.1-backups/$file.tar.bz2 2>&1 | tee mkrpm.log
+rpmbuild $2 -ta ../2.1-backups/$file.tar.bz2 2>&1 | tee mkrpm.log
 #rm -f /tmp/$file.tar.bz2
