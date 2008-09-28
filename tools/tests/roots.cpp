3edd97d2a0890ec26885599aa7c39b51e0e24708
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2008-09-28 20:52:27 +0900

    *** empty log message ***

--- tools/tests/roots.cpp
+++ tools/tests/roots.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- Copyright (C) 2002-2007 Kentaro Kitagawa
+ Copyright (C) 2002-2008 Kentaro Kitagawa
  kitag@issp.u-tokyo.ac.jp
 
  This program is free software; you can redistribute it and/or
