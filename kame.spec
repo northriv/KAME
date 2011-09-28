2bce9e0f93c2790abf39b0e7a79f11fcb88666d4
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2011-09-28 20:44:51 +0900

    Now works with Lion.

--- kame.spec
+++ kame.spec
@@ -7,7 +7,7 @@ Name: kame
 
 %{!?build_nidaqmx: %define build_nidaqmx 1}
 
-Version: 3.1.90
+Version: 3.1.95
 Release: 1
 License: GPL
 Group: Applications/Engineering
--- tools/mkport.sh
+++ tools/mkport.sh
@@ -13,7 +13,7 @@ portfile=../../macports/kde/kame/Portfile
 distdir=$dir/distfiles
 md5=`md5 ${tarfile} | sed -e 's/MD5.*= //'`
 cat $portfile | sed -e "s/checksums.*/checksums           md5 $md5/" -e "s/version             .*/version             $version/" > port.tmp && mv port.tmp $portfile
-sudo mkdir -p $distdir
+#sudo mkdir -p $distdir
 sudo cp -f $tarfile $distdir
 
 cmd="sudo port -v -n -k -u upgrade kame +debug_kame -debug -universal"
