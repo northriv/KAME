3a52af107d44527b633ed8b88a319432963a6783
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2013-02-03 20:23:07 +0900

    *** empty log message ***

--- modules/tempcontrol/tempcontrol.cpp
+++ modules/tempcontrol/tempcontrol.cpp
@@ -524,7 +524,7 @@ void XTempControl::createChannels(
 			lp->m_extDCSourceChannel, m_form->m_cmbExtDCSrcCh2, Snapshot( *lp->m_extDCSourceChannel));
 	}
 	else {
-//		m_form->m_toolBox->removeItem(1);
+		m_form->m_toolBox->removeItem(1);
 		m_form->m_pageLoop2->hide();
 	}
 	if(num_of_loops) {
@@ -556,7 +556,7 @@ void XTempControl::createChannels(
 			lp->m_extDCSourceChannel, m_form->m_cmbExtDCSrcCh, Snapshot( *lp->m_extDCSourceChannel));
 	}
 	else {
-//		m_form->m_toolBox->removeItem(0);
+		m_form->m_toolBox->removeItem(0);
 		m_form->m_pageLoop1->hide();
 	}
 }
