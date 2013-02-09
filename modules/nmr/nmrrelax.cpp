91a263c5a2944678dba6df018b842e098e3db272
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2013-02-09 21:00:21 +0900

    *** empty log message ***

--- modules/nmr/nmrrelax.cpp
+++ modules/nmr/nmrrelax.cpp
@@ -313,7 +313,7 @@ XNMRT1::obtainNextP1(Transaction &tr) {
 			bool p1dist_linear = (shot[ *p1Dist()].to_str() == P1DIST_LINEAR);
 			bool p1dist_log = (shot[ *p1Dist()].to_str() == P1DIST_LOG);
 			double k_0 = shot[ *this].m_sumpts.size() / log(p1max/p1min);
-			const auto &isigma = shot[ *this].m_sumpts.isigma;
+			const auto &sumpts = shot[ *this].m_sumpts.isigma;
 			for(;;) {
 				int mid;
 				if(p1dist_log)
@@ -331,10 +331,10 @@ XNMRT1::obtainNextP1(Transaction &tr) {
 				assert((mid >= lb) && (mid <= ub));
 				int isigma_0 = 0;
 				for(int idx = lb; idx < mid; ++idx)
-					isigma_0 += isigma[idx];
+					isigma_0 += sumpts[idx].isigma;
 				int isigma_1 = 0;
 				for(int idx = mid; idx < ub; ++idx)
-					isigma_1 += isigma[idx];
+					isigma_1 += sumpts[idx].isigma;
 				if(isigma_0 == isigma_1) {
 					if(randMT19937() < 0.5)
 						ub = mid;
