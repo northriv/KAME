d9ebcadc46b99cbd2f7afc728d24182d0a4e6041
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2013-02-20 17:55:11 +0900

    *** empty log message ***

--- modules/nmr/autolctuner.cpp
+++ modules/nmr/autolctuner.cpp
@@ -22,7 +22,7 @@ static const double TUNE_DROT_APPROACH = 5.0,
 	TUNE_DROT_FINETUNE = 2.0, TUNE_DROT_ABORT = 360.0; //[deg.]
 static const double TUNE_TRUST_APPROACH = 720.0, TUNE_TRUST_FINETUNE = 360.0; //[deg.]
 static const double TUNE_FINETUNE_START = 0.7; //-3dB@f0
-static const double TUNE_DROT_REQUIRED_N_SIGMA = 3.0;
+static const double TUNE_DROT_REQUIRED_N_SIGMA = 2.0;
 static const double SOR_FACTOR_MAX = 0.9;
 static const double SOR_FACTOR_MIN = 0.3;
 
@@ -391,9 +391,9 @@ XAutoLCTuner::analyze(Transaction &tr, const Snapshot &shot_emitter,
 	std::complex<double> ref_targeted;
 	switch(shot_this[ *this].mode) {
 	case Payload::TUNE_FINETUNE:
-		ref_targeted = reff0;
-		ref_sigma = reff0_sigma;
-		break;
+//		ref_targeted = reff0;
+//		ref_sigma = reff0_sigma;
+//		break;
 	case Payload::TUNE_APPROACHING:
 		ref_targeted = reffmin;
 		ref_sigma = reffmin_sigma;
@@ -509,6 +509,7 @@ XAutoLCTuner::analyze(Transaction &tr, const Snapshot &shot_emitter,
 		break;
 	}
 	double a = gamma * 2.0 * pow(std::norm(ref_targeted), gamma - 1.0);
+	// d |ref^2|^gamma / dCa,b
 	double drefgamma_dCa = a * (std::real(ref_targeted) * std::real(dref_dCa) + std::imag(ref_targeted) * std::imag(dref_dCa));
 	double drefgamma_dCb = a * (std::real(ref_targeted) * std::real(dref_dCb) + std::imag(ref_targeted) * std::imag(dref_dCb));
 
