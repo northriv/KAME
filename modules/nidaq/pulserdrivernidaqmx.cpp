29a49e4fcc56dccf3dec2a80109256b22d261338
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2013-04-08 23:29:04 +0900

    *** empty log message ***

--- modules/nidaq/pulserdrivernidaqmx.cpp
+++ modules/nidaq/pulserdrivernidaqmx.cpp
@@ -705,6 +705,7 @@ void *
 XNIDAQmxPulser::executeWriter(const atomic<bool> &terminating) {
  	double dma_do_period = resolution();
  	double dma_ao_period = resolutionQAM();
+	uint64_t written_total_do = 0, written_total_ao = 0;
 
  	unique_ptr<XThread<XNIDAQmxPulser> > th_genbuf;
 	//Starts a child thread generating patterns concurrently.
@@ -714,7 +715,6 @@ XNIDAQmxPulser::executeWriter(const atomic<bool> &terminating) {
 
 	try {
 		while( !terminating) {
-			uint64_t written_total_do = 0, written_total_ao = 0;
 			const tRawDO *pDO = m_patBufDO.curReadPos();
 			ssize_t samps_do = m_patBufDO.writtenSize();
 			const tRawAOSet *pAO = NULL;
@@ -731,12 +731,18 @@ XNIDAQmxPulser::executeWriter(const atomic<bool> &terminating) {
 			ssize_t written;
 			if(samps_ao > samps_do) {
 				written = writeToDAQmxAO(pAO, std::min(samps_ao, (ssize_t)m_transferSizeHintAO));
-				if(written) m_patBufAO.finReading(written);
+				if(written)
+					m_patBufAO.finReading(written);
+				else
+					msecsleep(lrint(resolutionQAM() * samps_ao));
 				written_total_ao += written;
 			}
 			else {
 				written = writeToDAQmxDO(pDO, std::min(samps_do, (ssize_t)m_transferSizeHintDO));
-				if(written) m_patBufDO.finReading(written);
+				if(written)
+					m_patBufDO.finReading(written);
+				else
+					msecsleep(lrint(resolution() * samps_do));
 				written_total_do += written;
 			}
 			if((written_total_do > m_preFillSizeDO) && ( !pAO || (written_total_ao > m_preFillSizeAO)))
