3d44030e86e0ddc5bb7f56615e659d0c1456cb39
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2013-04-12 19:13:48 +0900

    *** empty log message ***

--- modules/nidaq/pulserdrivernidaqmx.cpp
+++ modules/nidaq/pulserdrivernidaqmx.cpp
@@ -219,10 +219,10 @@ XNIDAQmxPulser::setupTasksDO(bool use_ao_clock) {
 			CHECK_DAQMX_RET(DAQmxSetDODataXferMech(m_taskDO, ch,
 												   DAQmx_Val_Interrupts));
 		}
-//		if(intfDO()->productFlags() & XNIDAQmxInterface::FLAG_BUGGY_XFER_COND_DO) {
+		if(intfDO()->productFlags() & XNIDAQmxInterface::FLAG_BUGGY_XFER_COND_DO) {
 			CHECK_DAQMX_RET(DAQmxSetDODataXferReqCond(m_taskDO, ch,
 													  DAQmx_Val_OnBrdMemNotFull));
-//		}
+		}
 	}
 
 	if(m_pausingBit) {
@@ -331,10 +331,10 @@ XNIDAQmxPulser::setupTasksAODO() {
 			CHECK_DAQMX_RET(DAQmxSetAODataXferMech(m_taskAO, ch,
 												   DAQmx_Val_Interrupts));
 		}
-//		if(intfAO()->productFlags() & XNIDAQmxInterface::FLAG_BUGGY_XFER_COND_AO) {
+		if(intfAO()->productFlags() & XNIDAQmxInterface::FLAG_BUGGY_XFER_COND_AO) {
 			CHECK_DAQMX_RET(DAQmxSetAODataXferReqCond(m_taskAO, ch,
 													  DAQmx_Val_OnBrdMemNotFull));
-//		}
+		}
 //		CHECK_DAQMX_RET(DAQmxSetAOReglitchEnable(m_taskAO, ch, false));
 	}
 	//Voltage calibration/ranges.
