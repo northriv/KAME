e4fd32ed1b8f351c1368a4018dd1ed0d24505d68
northriv <kitag@kochi-u.ac.jp>
2014-08-17 15:55:09 +0900

    d

--- modules/nmr/thamway/ezusbthamway.cpp
+++ modules/nmr/thamway/ezusbthamway.cpp
@@ -134,7 +134,7 @@ XWinCUSBInterface::XWinCUSBInterface(const char *name, bool runtime, const share
         for(Transaction tr( *this);; ++tr) {
             for(auto it = s_devices.begin(); it != s_devices.end(); ++it) {
                 XString idn = formatString("%s:%d", getIDN(it->handle, 7).c_str(), it->addr);
-                if(idn.empty()) continue;
+                if( !idn.length()) continue;
                 it->id = idn; //stores id string for open() to distinguish devices.
                 tr[ *device()].add(idn);
             }
