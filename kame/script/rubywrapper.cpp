6f8e32b11ff1aeb64c930a1e33de18f19790d002
northriv <kitag@kochi-u.ac.jp>
2014-08-31 19:47:33 +0900

    4.0.11

---
+++ kame/script/rubywrapper.cpp
@@ -0,0 +1,133 @@
+#include <ruby.h>
+#include "rubywrapper.h"
+
+static_assert(sizeof(VALUE) == sizeof(Ruby::Value), "Size mismatch for VALUE.");
+
+const int Ruby::Nil = Qnil;
+const int Ruby::False = Qfalse;
+const int Ruby::True = Qtrue;
+
+Ruby::Ruby(const char *scriptname) {
+	ruby_init();
+    ruby_script(scriptname);
+    ruby_init_loadpath();
+}
+Ruby::~Ruby() {
+    ruby_finalize();
+}
+int
+Ruby::evalProtect(const char* str) {
+	int state = 0;
+	rb_eval_string_protect(str, &state);
+    return state;
+}
+void
+Ruby::defineGlobalConst(const char *rbname, Value obj) {
+	rb_define_global_const(rbname, obj);
+}
+Ruby::Value
+Ruby::wrap_obj(Value cl, void *p, void (*f)(void *)) {
+	return Data_Wrap_Struct(cl, 0, f, p);
+}
+void *
+Ruby::unwrap_obj(Value self) {
+    wrapped_t *ptr;
+    Data_Get_Struct(self, wrapped_t, ptr);
+	return ptr;
+}
+
+void 
+Ruby::define_method(Value cl, const char *rbname, Value (*func)(...), int argnum) {
+    rb_define_method(cl, rbname, func, argnum);
+}
+void
+Ruby::define_singleton_method(Value obj, const char *rbname, Value (*func)(...), int argnum) {
+    rb_define_singleton_method(obj, rbname, func, argnum);
+}
+Ruby::Value
+Ruby::define_class(const char *rbname, Value super) {
+    Value c = rb_define_class(rbname, (super != Nil) ? super : rb_cObject);
+	rb_global_variable(&c);
+	return c;
+}
+void
+Ruby::emit_error(const char *errstr) {
+    rb_raise(rb_eRuntimeError, "%s", errstr);
+}
+
+template <>
+bool Ruby::isConvertible<const char*>(Value v) {
+    return TYPE(v) == T_STRING;
+}
+template <>
+bool Ruby::isConvertible<long>(Value v) {
+    return FIXNUM_P(v);
+}
+template <>
+bool Ruby::isConvertible<double>(Value v) {
+    return (TYPE(v) == T_FLOAT) || FIXNUM_P(v) || (TYPE(v) == T_BIGNUM);
+}
+template <>
+bool Ruby::isConvertible<bool>(Value v) {
+    return (TYPE(v) == T_TRUE) || (TYPE(v) == T_FALSE);
+}
+
+template <>
+const char* Ruby::convert(Value v) {
+    if( !isConvertible<const char*>(v))
+        throw "Type mismatch to STRING.";
+    return RSTRING_PTR(v);
+}
+template <>
+long Ruby::convert(Value v) {
+    if( !isConvertible<long>(v))
+        throw "Type mismatch to LONG.";
+    return FIX2LONG(v);
+}
+template <>
+double Ruby::convert(Value v) {
+    if( !isConvertible<double>(v))
+        throw "Type mismatch to NUM.";
+    return NUM2DBL(v);
+}
+template <>
+bool Ruby::convert(Value v) {
+    if( !isConvertible<bool>(v))
+        throw "Type mismatch to NUM.";
+    return (TYPE(v) == T_TRUE) ? true : false;
+}
+
+Ruby::Value Ruby::convertToRuby(const std::string &str) {
+    if(str.empty()) return rb_str_new2("");
+    return rb_str_new2(str.c_str());
+}
+
+template <>
+Ruby::Value Ruby::convertToRuby(int v) {
+    return INT2NUM(v);
+}
+
+template <>
+Ruby::Value Ruby::convertToRuby(unsigned int v) {
+    return UINT2NUM(v);
+}
+
+template <>
+Ruby::Value Ruby::convertToRuby(long v) {
+    return LONG2NUM(v);
+}
+
+template <>
+Ruby::Value Ruby::convertToRuby(unsigned long v) {
+    return ULONG2NUM(v);
+}
+
+template <>
+Ruby::Value Ruby::convertToRuby(double v) {
+    return rb_float_new(v);
+}
+
+template <>
+Ruby::Value Ruby::convertToRuby(bool v) {
+    return v ? Qtrue : Qfalse;
+}
---
+++ kame/script/rubywrapper.h
@@ -0,0 +1,212 @@
+#include <memory>
+#include <string>
+
+//! Wraps Ruby C interface and hides mysterious ruby.h from C++ libraries.
+class Ruby {
+private:
+    typedef std::pair<std::weak_ptr<void *>, std::weak_ptr<void *>> wrapped_t;
+public:
+	Ruby(const char *scriptname);
+	~Ruby();
+
+    static const int Nil;
+    static const int False;
+    static const int True;
+
+    //! \return state.
+    int evalProtect(const char* str);
+
+	typedef uintptr_t Value;
+	void defineGlobalConst(const char *rbname, Value obj);
+
+    //! C++ value to Ruby object
+    template <typename X>
+    static Value convertToRuby(X var);
+    static Value convertToRuby(const std::string &var);
+    //! Ruby object to C++ value
+    template <typename X>
+    static X convert(Value var);
+
+    template <typename X>
+    static bool isConvertible(Value var);
+
+	template <class P, class T>
+	struct Class {
+        Class(std::shared_ptr<P> parent, const char *rbname, Value super = Nil);
+        template<Value(P::*Func)(const std::shared_ptr<T>&)>
+        void defineSingletonMethod(Value obj, const char *rbname);
+        template<Value(P::*Func)(const std::shared_ptr<T>&,Value)>
+        void defineSingletonMethod(Value obj, const char *rbname);
+        template<Value(P::*Func)(const std::shared_ptr<T>&,Value,Value)>
+        void defineSingletonMethod(Value obj, const char *rbname);
+        template<Value(P::*Func)(const std::shared_ptr<T>&)>
+        void defineMethod(const char *rbname);
+        template<Value(P::*Func)(const std::shared_ptr<T>&,Value)>
+        void defineMethod(const char *rbname);
+        template<Value(P::*Func)(const std::shared_ptr<T>&,Value,Value)>
+        void defineMethod(const char *rbname);
+        Value rubyClassObject() const;
+        Value rubyObject(const std::shared_ptr<T> &obj) const;
+        static std::weak_ptr<T> unwrap(Value v) {
+            return unwrap_internal<Ptr>(v).second;
+        }
+    private:
+        typedef std::pair<std::weak_ptr<P>, std::weak_ptr<T>> Ptr;
+        static_assert(sizeof(Ptr) == sizeof(wrapped_t), "");
+        template<Value(P::*Func)(const std::shared_ptr<T>&)>
+        int create_function(Value(**func)(Value)) {
+            struct Func_t {
+                static Value __cdecl func_internal(Value self) {
+//            *func = [](Value self)->Value {
+                char errstr[256];
+					try {
+                        auto &st = unwrap_internal<Ptr>(self);
+                        std::shared_ptr<P> p(st.first);
+                        return (p.get()->*Func)(std::shared_ptr<T>(st.second));
+					}
+                    catch(std::bad_weak_ptr &) {
+                        snprintf(errstr, sizeof(errstr) - 1, "C object no longer exists.");}
+                    catch(std::string &e) {
+                        snprintf(errstr, sizeof(errstr) - 1, "%s", e.c_str());}
+                    catch(const char *e) {
+                        snprintf(errstr, sizeof(errstr) - 1, "%s", e);}
+                    emit_error(errstr); return Nil;
+                }
+            };
+            *func = &Func_t::func_internal;
+            return 0;
+		}
+        template<Value(P::*Func)(const std::shared_ptr<T>&, Value)>
+        int create_function(Value(**func)(Value, Value)) {
+            struct Func_t {
+                static Value __cdecl func_internal(Value self, Value x) {
+//            *func = [](Value self)->Value {
+                char errstr[256];
+                    try {
+                        auto &st = unwrap_internal<Ptr>(self);
+                        std::shared_ptr<P> p(st.first);
+                        return (p.get()->*Func)(std::shared_ptr<T>(st.second), x);
+                    }
+                    catch(std::bad_weak_ptr &) {
+                        snprintf(errstr, sizeof(errstr) - 1, "C object no longer exists.");}
+                    catch(std::string &e) {
+                        snprintf(errstr, sizeof(errstr) - 1, "%s", e.c_str());}
+                    catch(const char *e) {
+                        snprintf(errstr, sizeof(errstr) - 1, "%s", e);}
+                    emit_error(errstr); return Nil;
+                }
+            };
+            *func = &Func_t::func_internal;
+            return 1;
+        }
+        template<Value(P::*Func)(const std::shared_ptr<T>&, Value, Value)>
+        int create_function(Value(**func)(Value, Value, Value)) {
+            struct Func_t {
+                static Value __cdecl func_internal(Value self, Value x, Value y) {
+//            *func = [](Value self)->Value {
+                char errstr[256];
+                    try {
+                        auto &st = unwrap_internal<Ptr>(self);
+                        std::shared_ptr<P> p(st.first);
+                        return (p.get()->*Func)(std::shared_ptr<T>(st.second), x, y);
+                    }
+                    catch(std::bad_weak_ptr &) {
+                        snprintf(errstr, sizeof(errstr) - 1, "C object no longer exists.");}
+                    catch(std::string &e) {
+                        snprintf(errstr, sizeof(errstr) - 1, "%s", e.c_str());}
+                    catch(const char *e) {
+                        snprintf(errstr, sizeof(errstr) - 1, "%s", e);}
+                    emit_error(errstr); return Nil;
+                }
+            };
+            *func = &Func_t::func_internal;
+            return 2;
+        }
+        std::weak_ptr<P> m_parent;
+		Value m_rbObj;
+	};
+private:
+    template <class Y>
+    static Y &unwrap_internal(Value self) {
+        return *static_cast<Y*>(unwrap_obj(self));
+    }
+    static Value wrap_obj(Value cl, void *p, void (*)(void *));
+    static void *unwrap_obj(Value self);
+
+    static void define_method(Value cl, const char *rbname, Value (*func)(...), int argnum);
+    static void define_singleton_method(Value obj, const char *rbname, Value (*func)(...), int argnum);
+    static Value define_class(const char *rbname, Value super);
+
+    //!C++ objects should be destroyed before.
+    static void emit_error(const char *errstr);
+};
+
+template <class P, class T>
+Ruby::Class<P,T>::Class(std::shared_ptr<P> parent, const char *rbname, Value super) : m_parent(parent) {
+    m_rbObj = define_class(rbname, super);
+}
+template <class P, class T>
+template<Ruby::Value(P::*Func)(const std::shared_ptr<T>&)>
+void
+Ruby::Class<P,T>::defineMethod(const char *rbname) {
+    Value (*func)(Value);
+    typedef Value(*fp)(...);
+    int arg_num = create_function<Func>(&func);
+    define_method(m_rbObj, rbname, reinterpret_cast<fp>(func), arg_num);
+}
+template <class P, class T>
+template<Ruby::Value(P::*Func)(const std::shared_ptr<T>&,Ruby::Value)>
+void
+Ruby::Class<P,T>::defineMethod(const char *rbname) {
+    Value (*func)(Value,Value);
+    typedef Value(*fp)(...);
+    int arg_num = create_function<Func>(&func);
+    define_method(m_rbObj, rbname, reinterpret_cast<fp>(func), arg_num);
+}
+template <class P, class T>
+template<Ruby::Value(P::*Func)(const std::shared_ptr<T>&,Ruby::Value,Ruby::Value)>
+void
+Ruby::Class<P,T>::defineMethod(const char *rbname) {
+    Value (*func)(Value,Value,Value);
+    typedef Value(*fp)(...);
+    int arg_num = create_function<Func>(&func);
+    define_method(m_rbObj, rbname, reinterpret_cast<fp>(func), arg_num);
+}
+template <class P, class T>
+template<Ruby::Value(P::*Func)(const std::shared_ptr<T>&)>
+void
+Ruby::Class<P,T>::defineSingletonMethod(Value obj, const char *rbname) {
+    Value (*func)(Value);
+    typedef Value(*fp)(...);
+    int arg_num = create_function<Func>(&func);
+    define_singleton_method(obj, rbname, reinterpret_cast<fp>(func), arg_num);
+}
+template <class P, class T>
+template<Ruby::Value(P::*Func)(const std::shared_ptr<T>&,Ruby::Value)>
+void
+Ruby::Class<P,T>::defineSingletonMethod(Value obj, const char *rbname) {
+    Value (*func)(Value,Value);
+    typedef Value(*fp)(...);
+    int arg_num = create_function<Func>(&func);
+    define_singleton_method(obj, rbname, reinterpret_cast<fp>(func), arg_num);
+}
+template <class P, class T>
+template<Ruby::Value(P::*Func)(const std::shared_ptr<T>&,Ruby::Value,Ruby::Value)>
+void
+Ruby::Class<P,T>::defineSingletonMethod(Value obj, const char *rbname) {
+    Value (*func)(Value,Value,Value);
+    typedef Value(*fp)(...);
+    int arg_num = create_function<Func>(&func);
+    define_singleton_method(obj, rbname, reinterpret_cast<fp>(func), arg_num);
+}
+template <class P, class T>
+Ruby::Value
+Ruby::Class<P,T>::rubyClassObject() const {return m_rbObj;}
+template <class P, class T>
+Ruby::Value
+Ruby::Class<P,T>::rubyObject(const std::shared_ptr<T> &obj) const {
+    struct Deleter {
+        static void deleter(void *p) { delete (Ptr*)p;}
+    };
+    return wrap_obj(m_rbObj, new Ptr(m_parent, obj), &Deleter::deleter);
+}
