c1d6e5ee0a33eab4997d924793fef93c55388bb6
Kentaro KITAGAWA <kitag@kochi-u.ac.jp>
2015-05-13 23:50:05 +0900

    modified:   tools/kame.bat

--- tools/kame.bat
+++ tools/kame.bat
@@ -1,9 +1,22 @@
-rem @echo off
+@echo off
+:start
+set QTDIR=
 if not exist qtdir.txt (
- echo "Searching for QT...."
- dir /S/B C:\QT\ | findstr "mingw482_32\\bin\\Qt5Core.dll" >qtdir.txt
+ if "%QTROOT%"=="C:\QT\" (set QTROOT=E:\QT\) else if "%QTROOT%"=="E:\QT\" (set QTROOT=C:\)
+ echo Searching for QT DLLs in %QTROOT%....
+ dir /S/B %QTROOT% | findstr /r "mingw.*_32\\bin\\Qt5Core.dll" >qtdir.txt
 )
 set /p QTDIR= <qtdir.txt
+if "%QTDIR%"=="" (
+ echo "QT DLLs not found...."
+ del qtdir.txt
+ goto start
+)
+if not exist %QTDIR% (
+ echo "QT DLLs has lost...."
+ del qtdir.txt
+ goto start
+)
 set QTDIR=%QTDIR:\bin\Qt5Core.dll=%
 set PATH=%QTDIR%\bin;%PATH%
 kame.exe
