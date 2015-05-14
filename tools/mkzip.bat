798f9c729e9f5dd1919b2f6026a27b3e783bac77
northriv <kitag@kochi-u.ac.jp>
2015-05-14 14:26:49 +0900

    d

---
+++ tools/mkzip.bat
@@ -0,0 +1,9 @@
+copy *.dll ..\kame-win32\
+copy kame.exe ..\kame-win32\
+copy kame.bat ..\kame-win32\
+copy kame_*.qm ..\kame-win32\
+copy resources\* ..\kame-win32\Resources
+copy coremodules2\*.dll ..\kame-win32\coremodules2\
+copy coremodules\*.dll ..\kame-win32\coremodules\
+copy modules\*.dll ..\kame-win32\modules\
+remove ..\kame-win32\qtdir.txt
