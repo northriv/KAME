14b22a37ca5d2d037d545a6ee6bd6ec783d601e3
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2017-05-15 15:25:54 +0900

    simplify

--- modules/nmr/thamway/cyfxusb_libusb.cpp
+++ modules/nmr/thamway/cyfxusb_libusb.cpp
@@ -229,11 +229,6 @@ CyFXLibUSBDevice::open() {
 void
 CyFXLibUSBDevice::close() {
     if(handle) {
-        //hacks for stability.
-        libusb_clear_halt(handle, 0x2);
-        libusb_clear_halt(handle, 0x6);
-        libusb_clear_halt(handle, 0x8);
-        libusb_reset_device(handle);
         libusb_release_interface(handle,0);
         libusb_close(handle);
     }
