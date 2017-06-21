fb3f3e83b4802fde240ff3809626925490cf1ce7
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2017-06-21 12:34:52 +0900

    no message

--- modules/nmr/thamway/thamwaypulser.cpp
+++ modules/nmr/thamway/thamwaypulser.cpp
@@ -76,7 +76,8 @@ XThamwayPulser::XThamwayPulser(const char *name, bool runtime,
 	    for(unsigned int i = 0; i < sizeof(ports)/sizeof(int); i++) {
             tr[ *XPulser::portSel(i)] = ports[i];
 		}
-        tr[ *masterLevel()] = 3.0; //Use of the maximum power (45deg.) in IQ space.
+//        tr[ *masterLevel()] = 3.0; //Use of the maximum power (45deg.) in IQ space.
+        tr[ *masterLevel()] = -3.0;
         tr[ *aswSetup()] = 0.2;
     });
 }
