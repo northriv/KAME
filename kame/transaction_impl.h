b59e163c5aa3f5766f0035554c7aa968cf0f0d7d
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2017-07-14 23:28:30 +0900

    no message

--- kame/transaction_impl.h
+++ kame/transaction_impl.h
@@ -156,11 +156,11 @@ Node<XN>::PacketWrapper::print_() const {
 template <class XN>
 void
 Node<XN>::print_recoverable_error(const char* reason) {
-    fprintf(stderr, "Memory allocation has failed: %sTransaction is delaying...\n", reason);
+    fprintf(stderr, "Memory allocation has failed: %s\nTransaction is delaying...\n", reason);
 #ifdef gErrPrint
     try {
     char buf[256] = {};
-        snprintf(buf, sizeof(buf), "Memory allocation has failed: %sTransaction is delaying...\n", reason);
+        snprintf(buf, sizeof(buf), "Memory allocation has failed: %s\nTransaction is delaying...\n", reason);
         gErrPrint(buf);
     }
     catch (const std::bad_alloc &) {
