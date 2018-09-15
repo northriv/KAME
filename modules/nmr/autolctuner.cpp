25d5025b25e440d03227949b7f187d46627079bb
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2018-09-16 08:52:40 +0900

    no message

--- modules/nmr/autolctuner.cpp
+++ modules/nmr/autolctuner.cpp
@@ -225,7 +225,11 @@ LCRFit::fit(const std::vector<double> &s11, double fstart, double fstep, bool ra
             m_r1 = 2.0 * M_PI * f0org * l1() / q;
             omega_trust_scale = (retry % 8) / 6.0 * 2.0 + 0.5;
         }
-        if((fabs(r2()) > 10) || (c1() < 0) || (c2() < 0) || (qValue() > max_q) || (qValue() < 2)) {
+        if((fabs(r2()) > 10) || (c1() < 0) || (c2() < 0) || (qValue() > max_q * 2) || (qValue() < 2)) {
+            fprintf(stderr, "Fitting diverged.\n");
+            fprintf(stderr, "R1:%.3g, R2:%.3g, L:%.3g, C1:%.3g, C2:%.3g, Q:%.3g\n",
+                    r1(), r2(), l1(),
+                    c1(), c2(), qValue());
             randomize = true; //fitting diverged.
             continue;
         }
