55b2beda5e20f0639631753115812dd11e17aa38
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2018-11-20 02:22:53 +0900

    no message

--- modules/nmr/thamway/thamwayrealtimedso.cpp
+++ modules/nmr/thamway/thamwayrealtimedso.cpp
@@ -338,7 +338,7 @@ XThamwayPROT3DSO::executeAsyncRead(const atomic<bool> &terminated) {
 //            dbgPrint(formatString("asyncRead for %u initiated", (unsigned int)wridx));
 //            fprintf(stderr, "asyncRead for %u initiated\n", (unsigned int)wridx);
             while( !async->hasFinished() && !terminated)
-                msecsleep(20);
+                ; //event loop for libusb
             if(terminated) {
                 break;
             }
