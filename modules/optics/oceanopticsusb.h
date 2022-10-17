fa425653fd4e6636715eafc7d67b356c8764ecb1
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2022-10-17 18:56:38 +0900

    USB routines unified. TODO: individual initializations for Thamway/Oceanoptics

--- modules/optics/oceanopticsusb.h
+++ modules/optics/oceanopticsusb.h
@@ -14,106 +14,29 @@
 #ifndef OCEANOPTICSUSB_H
 #define OCEANOPTICSUSB_H
 
-#include "chardevicedriver.h"
-#include "charinterface.h"
+#include "cyfxusb.h"
 #include <vector>
 
-struct OceanOpticsUSBDevice {
-    OceanOpticsUSBDevice(const OceanOpticsUSBDevice&) = default;
-    virtual ~OceanOpticsUSBDevice() = default;
-
-    using List = std::vector<shared_ptr<OceanOpticsUSBDevice>>;
-    //! \return a list of connected OceanOptics USB devices.
-    static List enumerateDevices();
-
-    //! \arg buf be sure that the user buffer stays alive during an asynchronous IO.
-    int64_t bulkWrite(uint8_t ep, const uint8_t *buf, int len);
-    //! \arg ep 0x80 will be or-operated
-    //! \arg buf be sure that the user buffer stays alive during an asynchronous IO.
-    int64_t bulkRead(uint8_t ep, uint8_t* buf, int len);
-
-    virtual void open() = 0;
-    virtual void close() = 0;
-    virtual XString getString(int descid) = 0;
-
-    enum class CtrlReq : uint8_t  {
-        GET_STATUS = 0x00, CLEAR_FEATURE = 0x01, SET_FEATURE = 0x03, SET_ADDRESS = 0x05,
-        GET_DESCRIPTOR = 0x06, SET_DESCRIPTOR = 0x07, GET_CONFIGURATION = 0x08, SET_CONFIGURATION = 0x09,
-        GET_INTERFACE = 0x0A, SET_INTERFACE = 0x0B, SYNCH_FRAME = 0x0C, SET_SEL = 0x30,
-        USB_SET_ISOCH_DELAY = 0x31
-    };
-    enum class CtrlReqType : uint8_t {
-        STANDARD = (0x00 << 5),
-        CLASS = (0x01 << 5),
-        VENDOR = (0x02 << 5),
-        RESERVED = (0x03 << 5),
-        USB_RECIPIENT_DEVICE = 0x00,
-        USB_RECIPIENT_INTERFACE = 0x01,
-        USB_RECIPIENT_ENDPOINT = 0x02,
-        USB_RECIPIENT_OTHER = 0x03 };
-    virtual int controlWrite(CtrlReq request, CtrlReqType type, uint16_t value,
-                             uint16_t index, const uint8_t *buf, int len) = 0;
-    virtual int controlRead(CtrlReq request, CtrlReqType type, uint16_t value,
-                            uint16_t index, uint8_t *buf, int len) = 0;
-
-    unsigned int vendorID() const {return m_vendorID;}
-    unsigned int productID() const {return m_productID;}
-
-    class AsyncIO {
-    public:
-        AsyncIO() = default;
-        AsyncIO(const AsyncIO&) = delete;
-        AsyncIO(AsyncIO&&) noexcept = default;
-        virtual ~AsyncIO() = default;
-        void finalize(int64_t count_imm) {
-            m_count_imm = count_imm;
-        }
-        virtual bool hasFinished() const noexcept {return false;} //gcc doesn't accept pure virtual.
-        virtual int64_t waitFor() {return 0;} //gcc doesn't accept pure virtual.
-        //! \return true if a cancelation is successfully requested.
-        virtual bool abort() noexcept {return false;} //gcc doesn't accept pure virtual.
-
-        static XThreadLocal<std::vector<uint8_t>> stl_bufferGarbage;
-    protected:
-        int64_t m_count_imm = -1; //byte count of received user data, not incl. header.
-    };
-    virtual unique_ptr<AsyncIO> asyncBulkWrite(uint8_t ep, const uint8_t *buf, int len, unsigned int timeout_ms = 0) = 0;
-    virtual unique_ptr<AsyncIO> asyncBulkRead(uint8_t ep, uint8_t *buf, int len, unsigned int timeout_ms = 0) = 0;
-
-    XRecursiveMutex mutex;
-
-    enum {USB_DEVICE_DESCRIPTOR_TYPE = 1, USB_CONFIGURATION_DESCRIPTOR_TYPE = 2,
-        USB_STRING_DESCRIPTOR_TYPE = 3, USB_INTERFACE_DESCRIPTOR_TYPE = 4,
-        USB_ENDPOINT_DESCRIPTOR_TYPE = 5};
-protected:
-    OceanOpticsUSBDevice() = default;
-    uint16_t m_vendorID, m_productID;
-};
+struct OceanOpticsUSBDevice : public CyFXUSBDevice {};
 
 //! interfaces OceanOptics/SeaBreeze spectrometers
-class XOceanOpticsUSBInterface : public XCustomCharInterface {
+class XOceanOpticsUSBInterface : public XCyFXUSBInterface<OceanOpticsUSBDevice> {
     using USBDevice = OceanOpticsUSBDevice;
 public:
-    XOceanOpticsUSBInterface(const char *name, bool runtime, const shared_ptr<XDriver> &driver);
-    virtual ~XOceanOpticsUSBInterface();
+    XOceanOpticsUSBInterface(const char *name, bool runtime, const shared_ptr<XDriver> &driver)
+        : XCyFXUSBInterface<OceanOpticsUSBDevice>(name, runtime, driver) {
+        initialize();
+    }
 
-    virtual void open() override;
-    //! This can be called even if has already closed.
-    virtual void close() override;
-
-    void lock() {m_usbDevice->mutex.lock();} //!<overrides XInterface::lock().
-    void unlock() {m_usbDevice->mutex.unlock();}
-    bool isLocked() const {return m_usbDevice->mutex.isLockedByCurrentThread();}
+    virtual ~XOceanOpticsUSBInterface() {
+        finalize();
+    }
 
     virtual void send(const char *str) override {}
     virtual void receive() override {}
 
     void initDevice();
     void setIntegrationTime(unsigned int us);
-
-    unique_ptr<typename USBDevice::AsyncIO> asyncReceive(char *buf, ssize_t size);
-
-    virtual bool isOpened() const override {return !!usb();}
 
     int readSpectrum(std::vector<uint8_t> &buf, uint16_t pixels, bool usb_highspeed);
 
@@ -123,18 +46,12 @@ public:
     };
     InstrumConfig readConfigurations();
 protected:
-
-    const shared_ptr<USBDevice> &usb() const {return m_usbDevice;}
+    virtual DEVICE_STATUS examineDeviceBeforeFWLoad(const shared_ptr<CyFXUSBDevice> &dev) override;
+    virtual std::string examineDeviceAfterFWLoad(const shared_ptr<CyFXUSBDevice> &dev) override;
+    virtual XString gpifWave(const shared_ptr<CyFXUSBDevice> &dev) override {return {};}
+    virtual XString firmware(const shared_ptr<CyFXUSBDevice> &dev) override {return {};}
+    virtual void setWave(const shared_ptr<CyFXUSBDevice> &dev, const uint8_t *wave) override {}
 private:
-    shared_ptr<OceanOpticsUSBDevice> m_usbDevice;
-    static XMutex s_mutex;
-    static typename USBDevice::List s_devices;
-    static int s_refcnt;
-    unique_ptr<XThread> m_threadInit;
-    std::map<XString, shared_ptr<USBDevice>> m_candidates;
-    void openAllUSBdevices();
-    void closeAllUSBdevices();
-
     uint8_t m_ep_in_others = 1, m_ep_in_spec = 2, m_ep_in_spec_first1Kpixels = 6, m_ep_cmd = 1;
     enum class CMD {
         INIT=0x01, SET_INTEGRATION_TIME=0x02, SET_STROBE_ENABLE_STAT=0x03, SET_SHUTDOWN_MODE=0x04,QUERY_INFO=0x05,
