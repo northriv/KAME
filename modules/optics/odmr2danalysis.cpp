38a5de806b0703926b5e8f651c62cc944523b769
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2025-07-22 17:38:16 +0900

    no message

---
+++ modules/optics/odmr2danalysis.cpp
@@ -0,0 +1,366 @@
+/***************************************************************************
+        Copyright (C) 2002-2025 Kentaro Kitagawa
+		                   kitag@issp.u-tokyo.ac.jp
+		
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU Library General Public
+		License as published by the Free Software Foundation; either
+		version 2 of the License, or (at your option) any later version.
+		
+		You should have received a copy of the GNU Library General 
+		Public License and a list of authors along with this program; 
+		see the files COPYING and AUTHORS.
+***************************************************************************/
+//---------------------------------------------------------------------------
+#include "ui_odmr2danalysisform.h"
+#include "odmrfspectrum.h"
+#include "odmrimaging.h"
+#include "odmr2danalysis.h"
+#include "signalgenerator.h"
+
+#include "x2dimage.h"
+#include <QColorSpace>
+
+
+REGISTER_TYPE(XDriverList, ODMR2DAnalysis, "ODMR image analysis on frequency sweep");
+
+
+//---------------------------------------------------------------------------
+XODMR2DAnalysis::XODMR2DAnalysis(const char *name, bool runtime,
+    Transaction &tr_meas, const shared_ptr<XMeasure> &meas)
+    : XSecondaryDriver(name, runtime, ref(tr_meas), meas),
+    m_form(new FrmODMR2DAnalysis),
+    m_regionSelection(create<XComboNode>("RegionSelection", false, true)),
+    m_analysisMethod(create<XComboNode>("AnalysisMethod", false, true)),
+    m_average(create<XUIntNode>("Average", false)),
+    m_clearAverage(create<XTouchableNode>("ClearAverage", true)),
+    m_incrementalAverage(create<XBoolNode>("IncrementalAverage", false)),
+    m_autoMinMaxForColorMap(create<XBoolNode>("AutoMinMaxForColorMap", false)),
+    m_minForColorMap(create<XDoubleNode>("MinForColorMap", false)),
+    m_maxForColorMap(create<XDoubleNode>("MinForColorMap", false)),
+    m_colorMapMethod(create<XComboNode>("AnalysisMethod", false, true)),
+    m_processedImage(create<X2DImage>("ProcessedImage", false,
+                                      m_form->m_graphwidgetProcessed, m_form->m_edDump, m_form->m_tbDump, m_form->m_btnDump,
+                                      2, m_form->m_dblGamma,
+                                      m_form->m_tbMathMenu, meas, static_pointer_cast<XDriver>(shared_from_this()))) {
+
+    connect(odmrFSpectrum());
+
+    m_form->setWindowTitle(i18n("ODMR Image Analysis on Freq. Sweep - ") + getLabel() );
+
+    m_conUIs = {
+        xqcon_create<XQComboBoxConnector>(m_odmrFSpectrum, m_form->m_cmbODMRFSpectrum, ref(tr_meas)),
+        xqcon_create<XQSpinBoxUnsignedConnector>(average(), m_form->m_spbAverage),
+        xqcon_create<XQDoubleSpinBoxConnector>(m_minForColorMap, m_form->m_dblMinForColorMap),
+        xqcon_create<XQDoubleSpinBoxConnector>(m_maxForColorMap, m_form->m_dblMaxForColorMap),
+        xqcon_create<XQButtonConnector>(m_clearAverage, m_form->m_btnClearAverage),
+        xqcon_create<XQToggleButtonConnector>(m_incrementalAverage, m_form->m_ckbIncrementalAverage),
+        xqcon_create<XQToggleButtonConnector>(m_autoMinMaxForColorMap, m_form->m_ckbAutoMinMapForColorMap),
+        xqcon_create<XQComboBoxConnector>(m_analysisMethod, m_form->m_cmbAnalysisMethod, Snapshot( *m_analysisMethod)),
+        xqcon_create<XQComboBoxConnector>(m_colorMapMethod, m_form->m_cmbColorMapMethod, Snapshot( *m_colorMapMethod)),
+        xqcon_create<XQComboBoxConnector>(m_regionSelection, m_form->m_cmbSelRegion, Snapshot( *m_regionSelection)),
+    };
+
+    iterate_commit([=](Transaction &tr){
+        tr[ *average()] = 1;
+        tr[ *m_autoMinMaxForColorMap] = true;
+        tr[ *m_colorMapMethod].add({"RedWhiteBlue", "YellowGreenBlue"});
+        tr[ *m_analysisMethod].add({"CoG", "2nd Moment"});
+        tr[ *m_regionSelection].add({"All", "Sub Region"});
+    });
+
+    iterate_commit([=](Transaction &tr){
+        m_lsnOnClearAverageTouched = tr[ *clearAverage()].onTouch().connectWeakly(
+            shared_from_this(), &XODMR2DAnalysis::onClearAverageTouched);
+        m_lsnOnCondChanged = tr[ *average()].onValueChanged().connectWeakly(
+            shared_from_this(), &XODMR2DAnalysis::onCondChanged);
+        tr[ *m_minForColorMap].onValueChanged().connect(m_lsnOnCondChanged);
+        tr[ *m_maxForColorMap].onValueChanged().connect(m_lsnOnCondChanged);
+        tr[ *incrementalAverage()].onValueChanged().connect(m_lsnOnCondChanged);
+        tr[ *colorMapMethod()].onValueChanged().connect(m_lsnOnCondChanged);
+        tr[ *regionSelection()].onValueChanged().connect(m_lsnOnCondChanged);
+        tr[ *analysisMethod()].onValueChanged().connect(m_lsnOnCondChanged);
+    });
+}
+//---------------------------------------------------------------------------
+
+void
+XODMR2DAnalysis::showForms() {
+    m_form->showNormal();
+    m_form->raise();
+}
+
+void
+XODMR2DAnalysis::onCondChanged(const Snapshot &shot, XValueNodeBase *node) {
+    if(node == incrementalAverage().get())
+        trans( *average()) = 0;
+    if(node == incrementalAverage().get())
+        onClearAverageTouched(shot, clearAverage().get());
+    else
+        requestAnalysis();
+}
+void
+XODMR2DAnalysis::onClearAverageTouched(const Snapshot &shot, XTouchableNode *) {
+    trans( *this).m_timeClearRequested = XTime::now();
+    requestAnalysis();
+}
+bool
+XODMR2DAnalysis::checkDependency(const Snapshot &shot_this,
+    const Snapshot &shot_emitter, const Snapshot &shot_others,
+    XDriver *emitter) const {
+    shared_ptr<XODMRFSpectrum> fspectrum__ = shot_this[ *odmrFSpectrum()];
+    if( !fspectrum__) return false;
+    if((emitter != fspectrum__.get()) || (emitter == this))
+        return false;
+    const Snapshot &shot_fspectrum((emitter == fspectrum__.get()) ? shot_emitter : shot_others);
+    shared_ptr<XODMRImaging> odmr__ = shot_fspectrum[ *fspectrum__->odmr()];
+    if( !odmr__) return false;
+    shared_ptr<XSG> sg1__ = shot_fspectrum[ *fspectrum__->sg1()];
+    if( !sg1__) return false;
+    return true;
+}
+void
+XODMR2DAnalysis::analyze(Transaction &tr, const Snapshot &shot_emitter, const Snapshot &shot_others,
+    XDriver *emitter) {
+    const Snapshot &shot_this(tr);
+
+    shared_ptr<XODMRFSpectrum> fspectrum__ = shot_this[ *odmrFSpectrum()];
+    const Snapshot &shot_fspectrum((emitter == fspectrum__.get()) ? shot_emitter : shot_others);
+    const Snapshot &shot_odmr = shot_fspectrum;
+    shared_ptr<XODMRImaging> odmr__ = shot_fspectrum[ *fspectrum__->odmr()];
+
+    if(shot_odmr[ *odmr__->incrementalAverage()]) {
+        gWarnPrint(i18n("Do NOT use incremental avg. Skipping."));
+        throw XSkippedRecordError(__FILE__, __LINE__);
+    }
+
+    bool clear = (shot_this[ *this].m_timeClearRequested.isSet());
+    tr[ *this].m_timeClearRequested = {};
+
+    double freq = shot_fspectrum[ *fspectrum__].lastFreqAcquired() * 1e6; //MHz
+
+    double max__; //MHz
+    double min__; //MHz
+
+    if(shot_this[ *regionSelection()] == 0) {
+        //ALL
+        double cfreq = shot_fspectrum[ *fspectrum__->centerFreq()]; //MHz
+        double freq_span = shot_fspectrum[ *fspectrum__->freqSpan()] * 1e-3; //MHz
+        max__ = (cfreq + freq_span/2);
+        min__ = (cfreq - freq_span/2);
+    }
+    else {
+        double sub_max = shot_fspectrum[ *fspectrum__->subRegionMaxFreq()]; //MHz
+        double sub_min = shot_fspectrum[ *fspectrum__->subRegionMinFreq()]; //MHz
+        max__ = sub_max;
+        min__ = sub_min;
+    }
+    if(max__ <= min__) {
+        throw XSkippedRecordError(i18n("Invalid min. and max."), __FILE__, __LINE__);
+    }
+    if((freq < min__) || (freq > max__))
+        throw XSkippedRecordError(__FILE__, __LINE__);
+
+    constexpr int64_t coeff_freq_u16 = 0x10000LL;
+    uint16_t freq_u16 = lrint(coeff_freq_u16 * (freq - min__) / (max__ - min__));
+    tr[ *this].m_minValue = min__;
+
+    unsigned int width = shot_odmr[ *odmr__].width();
+    unsigned int height = shot_odmr[ *odmr__].height();
+
+    if( !tr[ *incrementalAverage()] && !clear && (emitter == fspectrum__.get())) {
+        clear = true;
+        if(std::max(1u, (unsigned int)tr[ *average()]) > tr[ *this].m_accumulated[0])
+            clear = false;
+    }
+    unsigned int num_summed_frames = tr[ *this].secondMoment() ? 3 : 2;
+    for(unsigned int i = 0; i < num_summed_frames; ++i) {
+        if( !tr[ *this].m_summedCounts[i] || (tr[ *this].m_summedCounts[i]->size() != width * height)) {
+            clear = true;
+        }
+    }
+
+    tr[ *this].m_width = width;
+    tr[ *this].m_height = height;
+    if(clear) {
+        tr[ *this].m_summedCounts[2]->clear();
+        for(unsigned int i = 0; i < num_summed_frames; ++i) {
+            tr[ *this].m_summedCounts[i] = summedCountsFromPool(width * height);
+            std::fill(tr[ *this].m_summedCounts[i]->begin(), tr[ *this].m_summedCounts[i]->end(), 0);
+            tr[ *this].m_accumulated[i] = 0;
+        }
+    }
+
+    constexpr int64_t coeff_PLOn_o_Off = 0x8000LL;
+
+    if(emitter == fspectrum__.get()) {
+        auto rawCountsPLOff = shot_odmr[ *odmr__].rawCountsPLOff();
+        auto rawCountsPLOn = shot_odmr[ *odmr__].rawCountsPLOn();
+
+        const uint32_t *summed_on_o_off = &tr[ *this].m_summedCounts[0]->at(0),
+            *summed_f_on_o_off = &tr[ *this].m_summedCounts[1]->at(0);
+        auto summedCountsNext_on_o_off = summedCountsFromPool(width * height);
+        auto summedCountsNext_f_on_o_off = summedCountsFromPool(width * height);
+        uint32_t *summedNext_on_o_off = &summedCountsNext_on_o_off->at(0);
+        uint32_t *summedNext_f_on_o_off = &summedCountsNext_f_on_o_off->at(0);
+
+        const uint32_t *pploff = &rawCountsPLOff->at(0);
+        const uint32_t *pplon = &rawCountsPLOn->at(0);
+
+        for(unsigned int y  = 0; y < height; ++y) {
+            for(unsigned int x  = 0; x < width; ++x) {
+                uint32_t ploff = *pploff++;
+                uint32_t plon = *pplon++;
+                int32_t plon_o_off_s32 = plon * coeff_PLOn_o_Off / (int64_t)ploff;
+                *summedNext_on_o_off++ = *summed_on_o_off++ + (uint32_t)plon_o_off_s32;
+                *summedNext_f_on_o_off++ = *summed_f_on_o_off++ + (uint32_t)(freq_u16 * plon_o_off_s32);
+            }
+        }
+        // CoG = <f dPL/PL> / <dPL/PL> = <f (on/off - 1)> / <on/off - 1> = (<f on/off> - <f>) / (<on/off> -  1)
+        tr[ *this].m_accumulated[0]++;
+        tr[ *this].m_accumulated[1] += freq_u16;
+        tr[ *this].m_summedCounts[0] = summedCountsNext_on_o_off;
+        tr[ *this].m_summedCounts[1] = summedCountsNext_f_on_o_off;
+        // tr[ *this].m_accumulated[2] += freqsq_u16;
+
+        // const uint32_t *summed_fsq_on_o_off = &tr[ *this].m_summedCounts[2]->at(0);
+        // auto summedCountsNext_fsq_on_o_off = summedCountsFromPool(width * height);
+    }
+
+    if( !shot_this[ *this].m_accumulated[0])
+        throw XSkippedRecordError(__FILE__, __LINE__); //visualize() will be called.
+
+    tr[ *this].m_coefficients[0] = 1.0 / coeff_PLOn_o_Off / tr[ *this].m_accumulated[0]; //for math tools
+    tr[ *this].m_coefficients[1] = tr[ *this].m_coefficients[0] / coeff_freq_u16 * (max__ - min__); //for math tools
+    // tr[ *this].m_coefficients[2] = tr[ *this].m_coefficients[0] / coeff_freqsq_u16; //for math tools
+
+    int64_t offsets[3];
+    offsets[0] = -1.0 / tr[ *this].m_coefficients[0]; //for dPL/PL.
+    offsets[1] = -tr[ *this].m_accumulated[1] * offsets[0];
+
+    if(tr[ *m_autoMinMaxForColorMap]) {
+        const uint32_t *summed_on_o_off = &tr[ *this].m_summedCounts[0]->at(0),
+            *summed_f_on_o_off = &tr[ *this].m_summedCounts[1]->at(0);
+
+        constexpr int64_t coeff_dCoG = 0x10000LL;
+        uint64_t dcogmin = 0xffffffffffffffffu;
+        uint64_t dcogmax = 0u;
+        for(unsigned int i  = 0; i < width * height; ++i) {
+            uint32_t on_o_off = *summed_on_o_off++;
+            uint32_t f_on_o_off = *summed_f_on_o_off++;
+            uint64_t dcog = ((int64_t)f_on_o_off - offsets[1]) * coeff_dCoG / ((int32_t)on_o_off - offsets[0]);
+            if(dcog > dcogmax)
+                dcogmax = dcog;
+            if(dcog < dcogmin)
+                dcogmin = dcog;
+        }
+        tr[ *minForColorMap()]  = (double)dcogmin / coeff_dCoG / coeff_freq_u16 * (max__ - min__) + min__;
+        tr[ *maxForColorMap()]  = (double)dcogmax / coeff_dCoG / coeff_freq_u16 * (max__ - min__) + min__;
+        tr.unmark(m_lsnOnCondChanged);
+    }
+    if(tr[ *incrementalAverage()]) {
+        tr[ *average()] = tr[ *this].m_accumulated[0];
+        tr.unmark(m_lsnOnCondChanged);
+        throw XSkippedRecordError(__FILE__, __LINE__); //visualize() will be called.
+    }
+    else {
+        if(tr[ *average()] > tr[ *this].m_accumulated[0])
+            throw XSkippedRecordError(__FILE__, __LINE__); //visualize() will be called.
+    }
+
+}
+
+void
+XODMR2DAnalysis::visualize(const Snapshot &shot) {
+    if( !shot[ *this].m_accumulated[0])
+        return;
+
+    unsigned int width = shot[ *this].width();
+    unsigned int height = shot[ *this].height();
+    auto qimage = std::make_shared<QImage>(width, height, QImage::Format_RGBA64);
+    qimage->setColorSpace(QColorSpace::SRgbLinear);
+
+    uint16_t *processed = reinterpret_cast<uint16_t*>(qimage->bits());
+    const uint32_t *summed_on_o_off = &shot[ *this].m_summedCounts[0]->at(0),
+        *summed_f_on_o_off = &shot[ *this].m_summedCounts[1]->at(0);
+
+    int64_t offsets[3];
+    offsets[0] = -1.0 / shot[ *this].m_coefficients[0]; //for dPL/PL.
+    offsets[1] = -shot[ *this].m_accumulated[1] * offsets[0];
+
+    uint64_t cogmin = shot[ *minForColorMap()];
+    uint64_t cogmax = shot[ *maxForColorMap()];
+
+    constexpr int64_t coeff_dCoG = 0x10000LL;
+    double df = shot[ *this].m_coefficients[1] / shot[ *this].m_coefficients[0];
+    double fmin = shot[ *this].minValue();
+
+    double c = 0x100000000LL * 0xffffLL * df / (cogmin - fmin) / coeff_dCoG ;
+    std::array<uint64_t, 3> coloroffsets = {};
+    uint64_t colorgain = lrint(0x100000000uLL * 0xffffuLL * df / (cogmax - cogmin) / coeff_dCoG); // /256 is needed for RGBA8888 format
+    std::array<int64_t, 3> dcog_gain = {};
+    switch((unsigned int)shot[ *m_colorMapMethod]) {
+    case 0:
+    default:
+        //RedWhiteBlue
+        dcog_gain[0] = lrint(colorgain / 2);
+        dcog_gain[1] = -dcog_gain[0];
+        dcog_gain[2] = -dcog_gain[0];
+        coloroffsets[0] = -0x100000000LL * 0xffffLL; // or 0xff
+        coloroffsets[1] = -coloroffsets[0];
+        coloroffsets[2] = -coloroffsets[0];
+        break;
+    case 1:
+        //YellowGreenBlue
+        dcog_gain[0] = lrint(colorgain);
+        dcog_gain[1] = -dcog_gain[0];
+        coloroffsets[0] = -0x100000000LL * 0xffffLL; // or 0xff
+        coloroffsets[1] = 0x100000000LL * 0x7fffLL; // or 0x7f
+        coloroffsets[2] = -coloroffsets[0];
+        break;
+    }
+
+    for(unsigned int i  = 0; i < width * height; ++i) {
+        uint32_t on_o_off = *summed_on_o_off++;
+        uint32_t f_on_o_off = *summed_f_on_o_off++;
+        uint64_t dcog = ((int64_t)f_on_o_off - offsets[1]) * coeff_dCoG / ((int32_t)on_o_off - offsets[0]);
+
+        for(unsigned int cidx: {0,1,2}) {
+            int64_t v = (dcog * dcog_gain[cidx] + coloroffsets[cidx]) / 0x100000000LL;
+            *processed++ = std::max(0LL, std::min(v, 0xffffLL));
+        }
+        *processed++ = 0xffffu;
+    }
+
+    std::vector<double> coeffs;
+    std::vector<const uint32_t *> rawimages;
+    for(unsigned int cidx: {0,1}) {
+        coeffs.push_back(shot[ *this].m_coefficients[cidx]);
+        rawimages.push_back( &shot[ *this].m_summedCounts[cidx]->at(0));
+    }
+    iterate_commit([&](Transaction &tr){
+        tr[ *this].m_qimage = qimage;
+        tr[ *m_processedImage->graph()->onScreenStrings()] = formatString("Avg:%u", (unsigned int)shot[ *this].m_accumulated[0]);
+        m_processedImage->updateImage(tr, qimage, rawimages, width, coeffs);
+    });
+}
+
+local_shared_ptr<std::vector<uint32_t>>
+XODMR2DAnalysis::summedCountsFromPool(int imagesize) {
+    local_shared_ptr<std::vector<uint32_t>> summedCountsNext, p;
+    //    for(int i = 0; i < NumSummedCountsPool; ++i) {
+    //        if( !m_summedCountsPool[i])
+    //            m_summedCountsPool[i] = make_local_shared<std::vector<uint32_t>>(imagesize);
+    //        p = m_summedCountsPool[i];
+    //        if(p.use_count() == 2) { //not owned by other threads.
+    //            summedCountsNext = p;
+    //            p->resize(imagesize);
+    //        }
+    //    }
+    if( !summedCountsNext)
+        summedCountsNext = make_local_shared<std::vector<uint32_t>>(imagesize);
+    return summedCountsNext;
+}
+
+
+
---
+++ modules/optics/odmr2danalysis.h
@@ -0,0 +1,131 @@
+/***************************************************************************
+        Copyright (C) 2002-2025 Kentaro Kitagawa
+		                   kitag@issp.u-tokyo.ac.jp
+		
+		This program is free software; you can redistribute it and/or
+		modify it under the terms of the GNU Library General Public
+		License as published by the Free Software Foundation; either
+		version 2 of the License, or (at your option) any later version.
+		
+		You should have received a copy of the GNU Library General 
+		Public License and a list of authors along with this program; 
+		see the files COPYING and AUTHORS.
+***************************************************************************/
+//---------------------------------------------------------------------------
+#ifndef ODMR2DANALYSIS_H
+#define ODMR2DANALYSIS_H
+
+
+#include "graph.h"
+#include "graphwidget.h"
+#include "xwavengraph.h"
+#include "secondarydriver.h"
+#include "xnodeconnector.h"
+
+#include <QPushButton>
+#include <QComboBox>
+#include <QCheckBox>
+
+class X2DImage;
+class XODMRFSpectrum;
+class QMainWindow;
+class Ui_FrmODMR2DAnalysis;
+typedef QForm<QMainWindow, Ui_FrmODMR2DAnalysis> FrmODMR2DAnalysis;
+
+
+class XWaveNGraph;
+class XXYPlot;
+
+class XODMR2DAnalysis : public XSecondaryDriver {
+public:
+    XODMR2DAnalysis(const char *name, bool runtime,
+        Transaction &tr_meas, const shared_ptr<XMeasure> &meas);
+    //! ususally nothing to do
+    virtual ~XODMR2DAnalysis() {}
+
+    //! Shows all forms belonging to driver
+    virtual void showForms() override;
+protected:
+    //! This function is called when a connected driver emit a signal
+    virtual void analyze(Transaction &tr, const Snapshot &shot_emitter, const Snapshot &shot_others,
+         XDriver *emitter) override;
+    //! This function is called after committing XPrimaryDriver::analyzeRaw() or XSecondaryDriver::analyze().
+    //! This might be called even if the record is invalid (time() == false).
+    virtual void visualize(const Snapshot &shot) override;
+    //! Checks if the connected drivers have valid time stamps.
+    //! \return true if dependency is resolved.
+    //! This function must be reentrant unlike analyze().
+    virtual bool checkDependency(const Snapshot &shot_this,
+        const Snapshot &shot_emitter, const Snapshot &shot_others,
+        XDriver *emitter) const override;
+public:
+    //! driver specific part below
+    struct Payload : public XSecondaryDriver::Payload {
+        // unsigned int numSamples() const {return m_summedCounts.size();}
+        double minValue() const {return m_minValue;}
+        unsigned int width() const {return m_width;}
+        unsigned int height() const {return m_height;}
+        bool secondMoment() const {return m_secondMoment;}
+    private:
+        friend class XODMR2DAnalysis;
+
+        //! 0x8000*PLon/PLoff, freq(0:fmin, 0xffff:fmax)*0x8000*on/off, (df^2 dPL/PL)
+        local_shared_ptr<std::vector<uint32_t>> m_summedCounts[3];
+        //! avg counts, sum freq_u16, sum f^2_u16.
+        unsigned int m_accumulated[3];
+        double m_coefficients[3];
+        double m_minValue;
+        bool m_secondMoment;
+        XTime m_timeClearRequested = {};
+        unsigned int m_width, m_height;
+        shared_ptr<QImage> m_qimage;
+    };
+
+
+    //! driver specific part below
+    const shared_ptr<XComboNode> &regionSelection() const {return m_regionSelection;}
+    const shared_ptr<XComboNode> &analysisMethod() const {return m_analysisMethod;}
+
+    const shared_ptr<XUIntNode> &average() const {return m_average;} //
+    const shared_ptr<XTouchableNode> &clearAverage() const {return m_clearAverage;}
+    const shared_ptr<XBoolNode> &incrementalAverage() const {return m_incrementalAverage;}
+
+    const shared_ptr<XBoolNode> &autoMinMaxForColorMap() const {return m_autoMinMaxForColorMap;}
+    const shared_ptr<XDoubleNode> &minForColorMap() const {return m_minForColorMap;}
+    const shared_ptr<XDoubleNode> &maxForColorMap() const {return m_maxForColorMap;}
+    const shared_ptr<XComboNode> &colorMapMethod() const {return m_colorMapMethod;}
+
+    const shared_ptr<X2DImage> &processedImage() const {return m_processedImage;}
+
+    const shared_ptr<XItemNode<XDriverList, XODMRFSpectrum>> &odmrFSpectrum() const {return m_odmrFSpectrum;}
+protected:
+    shared_ptr<Listener> m_lsnOnClearAverageTouched, m_lsnOnCondChanged;
+
+    const qshared_ptr<FrmODMR2DAnalysis> m_form;
+
+    const shared_ptr<XItemNode<XDriverList, XODMRFSpectrum> > m_odmrFSpectrum;
+    const shared_ptr<XComboNode> m_regionSelection;
+    const shared_ptr<XComboNode> m_analysisMethod;
+    const shared_ptr<XUIntNode> m_average;
+    const shared_ptr<XTouchableNode> m_clearAverage;
+    const shared_ptr<XBoolNode> m_incrementalAverage;
+    const shared_ptr<XBoolNode> m_autoMinMaxForColorMap;
+    const shared_ptr<XDoubleNode> m_minForColorMap;
+    const shared_ptr<XDoubleNode> m_maxForColorMap;
+    const shared_ptr<XComboNode> m_colorMapMethod;
+
+    const shared_ptr<X2DImage> m_processedImage;
+
+private:
+    void onClearAverageTouched(const Snapshot &shot, XTouchableNode *);
+    void onCondChanged(const Snapshot &shot, XValueNodeBase *);
+
+    std::deque<xqcon_ptr> m_conUIs;
+
+    constexpr static unsigned int NumSummedCountsPool = 5;
+    atomic_shared_ptr<std::vector<uint32_t>> m_summedCountsPool[NumSummedCountsPool];
+    local_shared_ptr<std::vector<uint32_t>> summedCountsFromPool(int imagebytes);
+
+};
+
+#endif
