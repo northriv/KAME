494fa285117b1792f06322e4ecc6e6742944a1d6
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2025-10-26 23:10:03 +0900

    no message

--- kame/script/notebook/notebook_kame_kernel_manager.py
+++ kame/script/notebook/notebook_kame_kernel_manager.py
@@ -59,7 +59,6 @@ class KAMENotebookKernelManager(MappingKernelManager):
         kernel.control_port = 0
         kernel.load_connection_file(self.connfile)
 
-
     async def start_kernel(self, **kwargs):
         """Overrides ``MappingKernelManager.start_kernel``. Connects
         all new kernels to the IPython kernel specified by ``connfile``.
@@ -73,3 +72,4 @@ class KAMENotebookKernelManager(MappingKernelManager):
     def restart_kernel(self, *args, **kwargs):
         """Overrides ``MappingKernelManager.restart_kernel``. Does nothing. """
         pass
+
