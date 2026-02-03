ae0501a62facf12c977a9c278b4c7ba3eda6280a
northriv <kitagawa@phys.s.u-tokyo.ac.jp>
2026-02-04 00:14:17 +0900

    no message

--- modules/sg/usersignalgenerator.cpp
+++ modules/sg/usersignalgenerator.cpp
@@ -134,8 +134,8 @@ XAgilentSGSCPI::XAgilentSGSCPI(const char *name, bool runtime,
 	Transaction &tr_meas, const shared_ptr<XMeasure> &meas)
     : XCharDeviceDriver<XSG>(name, runtime, ref(tr_meas), meas) {
 //	interface()->setGPIBUseSerialPollOnWrite(false);
-    interface()->setGPIBWaitBeforeWrite(10);
-    interface()->setGPIBWaitBeforeRead(30);
+//	interface()->setGPIBWaitBeforeWrite(10);
+//	interface()->setGPIBWaitBeforeRead(10);
     trans( *sweepMode()).add({"Off", "Freq.", "Ampl.", "Ampl Alt.", "Ampl 112 Alt.", "Ampl 1112 Alt."});
 }
 XHP8664::XHP8664(const char *name, bool runtime,
